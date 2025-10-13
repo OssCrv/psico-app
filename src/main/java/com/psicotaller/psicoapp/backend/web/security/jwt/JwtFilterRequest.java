589d790ca8705aac6c627136231de1d021d92996
Ossman O. C. <carvajalossman@gmail.com>
2025-10-13 09:19:56 -0500

    Añadiendo espacio a 'Bearer_'

--- src/main/java/com/psicotaller/psicoapp/backend/web/security/jwt/JwtFilterRequest.java
+++ src/main/java/com/psicotaller/psicoapp/backend/web/security/jwt/JwtFilterRequest.java
@@ -32,7 +32,7 @@ public class JwtFilterRequest extends OncePerRequestFilter {
     )
             throws ServletException, IOException {
         String authHeader = request.getHeader("Authorization");
-        if (authHeader != null && authHeader.startsWith("Bearer")) {
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
             String jwt = authHeader.substring(7);
             String username = jwtManager.extractUsername(jwt);
 
